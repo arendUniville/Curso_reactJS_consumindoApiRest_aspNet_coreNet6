f83bc487392d7860e56008bd3ab5a93329ddf624
Arend <matheus.arend@univille.br>
2023-05-19 15:17:21 -0300

    47 - Iniciando implementação da tela books

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login';
-
+import Book from './pages/Book';
 
 export default function Rotas() {
     return (
         <BrowserRouter>
             <Routes>
 
-                <Route path="/" element={<Login />} />
+                <Route path="/" exact element={<Login />} />
+                <Route path="/book" element={<Book />} />
 
             </Routes>
 
