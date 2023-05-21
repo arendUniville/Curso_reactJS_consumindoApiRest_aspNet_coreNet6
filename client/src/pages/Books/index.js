e9e94a24bda4344847e917f91580b986733c51a5
Arend <matheus.arend@univille.br>
2023-05-21 12:34:42 -0300

    57 - Integrando a tela de listagem de livros

--- client/src/pages/Books/index.js
+++ client/src/pages/Books/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
 
+
+import api from '../../services/api';
 
 import './styles.css';
 import logoImage from '../../assets/logo.svg';
 
 
 
-export default function Books()
-{
+export default function Books() {
+
+
+    const [books, setBooks] = useState([]);
+
+
+    const userName = localStorage.getItem('userName');
+    const accessToken = localStorage.getItem('accessToken');
+
+
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+
+        api.get('api/Book/v1/asc/5/1', {
+
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+
+            }
+        }).then(response => {
+            setBooks(response.data.list)
+        })
+
+    }, [accessToken]);
 
     return (
 
@@ -19,7 +45,7 @@ export default function Books()
 
                 <img src={logoImage} alt="Erudio" />
 
-                <span>Welcome, <strong>Arend</strong>!</span>
+                <span>Welcome, <strong>{userName.toLowerCase()}</strong>!</span>
 
 
                 <Link className="button" to="new">Add new book</Link>
@@ -36,156 +62,45 @@ export default function Books()
 
             <ul>
 
-                <li>
+                {books.map(book => (
 
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
+                    <li key={book.id}>
 
+                        <strong>Title:</strong>
+                        <p>{book.title}</p>
 
-                    <strong>Author:</strong>
-                    <p>Matheus Arend</p>
 
+                        <strong>Author:</strong>
+                        <p>{book.author}</p>
 
-                    <strong>Price:</strong>
-                    <p>R$ 49,90</p>
 
+                        <strong>Price:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(book.price)}</p>
 
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
 
+                        <strong>Release Date:</strong>
+                        <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
 
 
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
+                        <button type="button">
+                            <FiEdit size={20} color="#251FC5" />
+                        </button>
 
-                </li>
+                        <button type="button">
+                            <FiTrash2 size={20} color="#251FC5" />
+                        </button>
 
-                <li>
+                    </li>
 
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
 
-
-                    <strong>Author:</strong>
-                    <p>Matheus Arend</p>
-
-
-                    <strong>Price:</strong>
-                    <p>R$ 49,90</p>
-
-
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-
-                </li>
-
-                <li>
-
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-
-
-                    <strong>Author:</strong>
-                    <p>Matheus Arend</p>
-
-
-                    <strong>Price:</strong>
-                    <p>R$ 49,90</p>
-
-
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-
-                </li>
-
-                <li>
-
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-
-
-                    <strong>Author:</strong>
-                    <p>Matheus Arend</p>
-
-
-                    <strong>Price:</strong>
-                    <p>R$ 49,90</p>
-
-
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-
-                </li>
-
-                <li>
-
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-
-
-                    <strong>Author:</strong>
-                    <p>Matheus Arend</p>
-
-
-                    <strong>Price:</strong>
-                    <p>R$ 49,90</p>
-
-
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-
-                </li>
+                ))}
 
             </ul>
 
 
         </div>
 
-        );
+    );
 
 }
