57f93ce03f625ca27a14be59cc68e6aa29cb1ff7
Arend <matheus.arend@univille.br>
2023-05-21 11:48:47 -0300

    55 - Integrando a tela de login com a API

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import api from '../../services/api';
+
 import './styles.css';
 
 import logoImage from '../../assets/logo.svg';
@@ -6,6 +10,47 @@ import padlock from '../../assets/padlock.png';
 
 
 export default function Login() {
+
+	const [userName, setUserName] = useState('');
+	const [password, setPassword] = useState('');
+
+	const navigate = useNavigate();
+
+
+	async function login(e) {
+
+		e.preventDefault();
+
+		const data = {
+
+			userName,
+			password,
+
+		};
+
+
+		try
+		{
+
+			const response = await api.post('api/auth/v1/signin', data);
+
+
+			localStorage.setItem('userName', userName);
+			localStorage.setItem('accessToken', response.data.accessToken);
+			localStorage.setItem('refreshToken', response.data.refreshToken);
+
+
+			navigate("/books");
+
+
+		} catch (error)
+		{
+			alert('Login failed! Try again.');
+		}
+
+
+	}
+
 
 	return (
 
@@ -16,12 +61,12 @@ export default function Login() {
 				<img src={logoImage} alt="Erudio Logo" />
 
 
-				<form>
+				<form onSubmit={login}>
 
 					<h1>Access your account</h1>
 
-					<input placeholder="Username" />
-					<input type="password" placeholder="Password" />
+					<input placeholder="Username" value={userName} onChange={ e => setUserName(e.target.value) } />
+					<input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
 
 					<button type="submit" className="button">Login</button>
 
